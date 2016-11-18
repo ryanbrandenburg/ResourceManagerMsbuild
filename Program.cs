f9729f9cd5d03e4310f5f4a3ea493bcf837bc5f8
Ryan Brandenburg <rybrande@microsoft.com>
2016-11-18 15:00:28 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ResourceManager
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var resourcesManager = new System.Resources.ResourceManager("ResourceManager.Resources.Strings", typeof(Program).GetTypeInfo().Assembly);
+
+            var result = resourcesManager.GetString("key1", CultureInfo.CurrentUICulture);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
